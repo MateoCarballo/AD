d9a1ff2c40173878c4eccde0c973a064dc7d0d57
Mateo <mateocarballo2@gmail.com>
2024-09-26 21:12:29 +0200

    Completada tarea 104

--- Ficheros/FicherosJava/src/Main.java
+++ Ficheros/FicherosJava/src/Main.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 
@@ -9,7 +6,9 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 //        ejercicio101();
-        ejercicio102();
+//        ejercicio102();
+            ejercicio104();
+//        probarRutas();
     }
 
     static void ejercicio101() throws IOException {
@@ -73,6 +72,77 @@ public class Main {
                listarContenido(new File(f.getPath() + "/" + f.list()[i]));
             }
         }
+    }
+
+    static void ejercicio104() throws IOException {
+        /*
+        Desarrolla un programa Java que permita:
+        Crear un fichero de texto de forma secuencial en el que vaya el siguiente texto:
+        Ejemplo de escritura en un fichero de texto o txt
+        Visualizar el contenido del fichero
+         */
+
+        String cadenaParaEscribir = "Ejemplo de escritura en un fichero de texto o txt";
+        ej104CopiarString(cadenaParaEscribir);
+        ej104leerArchivo("FicherosJava\\src\\FicherosPrueba\\Ficheros104\\Fichero.txt");
+
+    }
+
+    private static void ej104leerArchivo(String rutaAlArchivo) {
+        try {
+            File f = new File(rutaAlArchivo);
+            FileReader fr = new FileReader(f);
+
+            /*char b[]=new char[150];
+
+            fr.read(b);
+            for (int i = 0; i < b.length; i++){
+                System.out.print((char)b[i]);
+            }*/
+            //Lectura caracter a carácter hasta llegar al fin
+            int i;
+            while((i = fr.read()) != -1){
+                System.out.print((char) i);
+            }
+
+            fr.close();
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo");;
+        }
+
+
+    }
+
+    static void ej104CopiarString(String cadenaParaCopiar){
+        String[] vectorCadena = cadenaParaCopiar.split(" ");
+        BufferedWriter bw= null;
+        try {
+            File archivo = new File("FicherosJava\\src\\FicherosPrueba\\Ficheros104\\Fichero.txt");
+            if (!archivo.exists()){
+                archivo.createNewFile();
+            }
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(archivo), "UTF-8"));
+
+            for(String c :vectorCadena ){
+                bw.write(c+" ");
+            }
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("Error durante la escritura del rutaAlArchivo");
+        }
+
+    }
+
+    static void probarRutas() throws IOException {
+        File f = new File("FileConRutaRelativa");
+
+        File[] listaFicheros = f.listFiles();
+
+        System.out.println(f.getParent());
+//        for (int i = 0; i < listaFicheros.length ; i++) {
+//            System.out.println(listaFicheros[i].getName());
+//        }
+        System.out.println();
     }
 
 }
