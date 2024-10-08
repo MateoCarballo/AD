401b95380c53a836a59d346f641f1a6a1243877d
Mateo <mateocarballo2@gmail.com>
2024-10-08 11:38:46 +0200

    Copiada base del proyecto y lanzada ventana principal

---
+++ Entregable JSON/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,13 @@
+
+O
+src/gui/VentanaBorrarAutor.java,3\4\34097cb4a65be2e3c204535dad3ff19d7dfa61d3
+Q
+!src/gui/VentanaCambiarTitulo.java,6\7\672a7b0d3d62611999ee2ae201a177e0dcd2abc5
+N
+src/gui/VentanaCrearAutor.java,e\d\ed4232b87b8cb05a08a1da5320c92270a1cb407d
+H
+src/model/Principal.java,f\4\f4ce3f9250064def1381b776e9de037960251fdf
+P
+ src/model/AplicacionAutores.java,9\f\9f0ed300145588651c1a709e3e991566da8074c3
+P
+ src/gui/VentanaInicioSesion.java,d\6\d6c9fbd07df7f947312c461dd69584034bc13fb7
---
+++ Entregable JSON/src/model/AplicacionAutores.java
@@ -0,0 +1,96 @@
+package model;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import javax.swing.JOptionPane;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import gui.VentanaBorrarAutor;
+import gui.VentanaCambiarTitulo;
+import gui.VentanaCrearAutor;
+import gui.VentanaInicioSesion;
+import gui.VentanaMenuAutor;
+import gui.VentanaVerDatos;
+
+public class AplicacionAutores
+{
+
+	private final String RUTA_FICHERO = "";
+	private VentanaInicioSesion ventanaInicioSesion;
+	private VentanaCrearAutor ventanaCrearAutor;
+	private VentanaMenuAutor ventanaMenuAutor;
+	private VentanaVerDatos ventanaVerDatos;
+	private VentanaCambiarTitulo ventanaCambiarTitulo;
+	private VentanaBorrarAutor ventanaBorrarAutor;
+
+	private void crearFicheroJson()	{
+		// TODO
+	}
+
+	private void guardarFicheroJson(JSONArray autores) {
+		// TODO
+	}
+//
+//	private JSONArray obtenerAutoresJson() {
+//		// TODO
+//	}
+//
+//	private int obtenerPosicionAutor(String nombreAutor, JSONArray autores){
+//		// TODO
+//	}
+//
+//	private JSONObject obtenerAutoresJson(String nombreAutor){
+//		// TODO
+//	}
+
+	public void ejecutar(){
+		// TODO
+		this.ventanaInicioSesion = new VentanaInicioSesion(this);
+		ventanaInicioSesion.setVisible(true);
+	}
+
+	public void iniciarValidacion(String nombreAutor, String tituloLibroAutor){
+		// TODO
+	}
+
+	public void cerrarSesion(){
+		// TODO
+	}
+
+	public void crearAutor(String nombre, String titulo, String paginas, String editorial){
+		// TODO
+	}
+
+	public void cambiarTituloLibro(String nombreAutor, String nuevoTitulo){
+		// TODO
+	}
+
+	public void borrarAutor(String nombreAutor){
+		// TODO
+	}
+
+	public void mostrarVentanaCrearAutor(){
+		// TODO
+	}
+
+	public void mostrarVentanaVerDatos(String nombreAutor){
+		// TODO
+	}
+
+	public void mostrarVentanaCambiarTitulo(String nombreAutor){
+		// TODO
+	}
+
+	public void mostrarVentanaBorrarAutor(String nombreAutor){
+		// TODO
+	}
+
+	public void mostrarMenuAutor(String nombreAutor){
+		// TODO
+	}
+}
