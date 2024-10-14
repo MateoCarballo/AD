0ebfec9a4f46539457063302e5e5e651b2bf953c
Mateo <mateocarballo2@gmail.com>
2024-10-14 19:20:01 +0200

    A medias con la preparacion

--- Preparacion examen/src/Main.java
+++ Preparacion examen/src/Main.java
@@ -1,19 +1,132 @@
 import java.io.*;
+import java.sql.SQLOutput;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
         final String RUTA = "src/Archivos";
-        final String NOMBRE_ARCHIVO = "Archivo de texto2.txt";
-        final String NOMBRE_ARCHIVO2 = "Archivo de texto.txt";
+        final String NOMBRE_ARCHIVO_FILEWRITER = "Escrito con FileWriter.txt";
+        final String NOMBRE_ARCHIVO_BUFFERWRITER = "Escrito con BufferWriter.txt";
+        final String NOMBRE_ARCHIVO_DATAOUTPUTSTREAM = "Escrito con DataOutputStream.txt";
 
+        //CREACION DEL DIRECTORIO
         crearDirectorio(RUTA);
-        crearFichero(RUTA,NOMBRE_ARCHIVO);
-        escribirSecuencial("Esta cadena la escribo secuancialmente",RUTA+"/"+NOMBRE_ARCHIVO);
-        escribirAleatorio("Esta cadena la escribo aleatoriamente",RUTA+"/"+NOMBRE_ARCHIVO);
-        leerArchivo(RUTA+"/"+NOMBRE_ARCHIVO2);
+
+        //CREACION DE LOS ARCHIVOS
+        crearFichero(RUTA,NOMBRE_ARCHIVO_FILEWRITER);
+        crearFichero(RUTA,NOMBRE_ARCHIVO_BUFFERWRITER);
+        crearFichero(RUTA,NOMBRE_ARCHIVO_DATAOUTPUTSTREAM);
+
+        //ESCRIBIR ARCHIVOS CON FILEWRITER
+        escribirArchivoFileWriter(RUTA + File.separator + NOMBRE_ARCHIVO_FILEWRITER );
+        escribirArchivoBufferedWriter(RUTA + File.separator + NOMBRE_ARCHIVO_BUFFERWRITER);
+        escribirArchivoDataOutputStream(RUTA + File.separator + NOMBRE_ARCHIVO_DATAOUTPUTSTREAM);
+
+        //LEER ARCHIVOS CON LOS EQUIVALENTES A LA CLASE USADA PARA ESCRIBIR
+
+        leerArchivoFileReader(new File(RUTA + File.separator + NOMBRE_ARCHIVO_FILEWRITER));
+        leerArchivoBufferedReader(new File(RUTA + File.separator + NOMBRE_ARCHIVO_FILEWRITER));
+        leerArchivoDataInputStream(new File(RUTA + File.separator + NOMBRE_ARCHIVO_FILEWRITER));
+    }
+
+    private static void leerArchivoDataInputStream(File file) {
+
+        try (DataInputStream dis = new DataInputStream(new FileInputStream(file))){
+
+            System.out.println("*************************** LECTURA CON DATAINPUTSTREAM ***********************");
+            dis.readChar();
+        }catch (IOException e){
+            System.out.println("Error durante la lectura");
+        }
+    }
+
+    private static void leerArchivoBufferedReader(File file) {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))){
+            System.out.println("*************************** LECTURA CON BUFFEREDREADER ***********************");
+            System.out.println(br.readLine());
+            System.out.println(br.readLine());
+            System.out.println(br.readLine());
+            System.out.println(br.readLine());
+            System.out.println(br.readLine());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void leerArchivoFileReader(File file) {
+
+        try(FileReader fr = new FileReader(file)) {
+            int lecturaEnInt = fr.read();
+            System.out.print((char)lecturaEnInt);
+
+            while (lecturaEnInt != -1){
+                lecturaEnInt= fr.read();
+                System.out.print((char) lecturaEnInt);
+            }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
+
+    private static void escribirArchivoDataOutputStream(String ruta) {
+        File f = new File(ruta);
+
+        try(DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(f)))){
+            dos.writeUTF("Esto es un char introducido como int"+"\n");
+            dos.writeChar(100);
+            dos.writeUTF("Esto es un int"+"\n");
+            dos.writeInt(2);
+            dos.writeUTF("Esto es un boolean");
+            dos.writeBoolean(true);
+        }catch (IOException e){
+            System.out.println("Error durante la escritura con BufferedWriter");
+        }
+    }
+
+    private static void escribirArchivoBufferedWriter(String ruta) {
+        File f = new File(ruta);
+        char [] vectorChar = {'V','E','C','T','O','R'};
+        String [] vectorString = {"Vector","de","Strings"};
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(f))){
+            bw.write("Esto es un String"+"\n");
+            bw.write("Cadena");
+            bw.write("Esto es un int"+"\n");
+            bw.write(5);
+            bw.write("Esto es un vector de chars"+"\n");
+            bw.write(vectorChar+"\n");
+            bw.write("Esto es un vector de Strings"+"\n");
+            bw.write(Arrays.toString(vectorString)+"\n");
+
+        }catch (IOException e){
+            System.out.println("Error durante la escritura con BufferedWriter");
+        }
+    }
+
+    private static void escribirArchivoFileWriter(String ruta) {
+        File f = new File(ruta);
+        char [] vectorChar = {'V','E','C','T','O','R'};
+        String [] vectorString = {"Vector","de","Strings"};
+        try (FileWriter fw = new FileWriter((f))){
+            fw.write("Esto es un String"+"\n");
+            fw.write("Cadena");
+            fw.write("Esto es un int"+"\n");
+            fw.write(5);
+            fw.write("Esto es un vector de chars"+"\n");
+            fw.write(vectorChar+"\n");
+            fw.write("Esto es un vector de Strings"+"\n");
+            fw.write(Arrays.toString(vectorString)+"\n");
+
+        }catch (IOException e){
+            System.out.println("Error durante la escritura");
+        }
+
+    }
+
     public static void crearDirectorio(String rutaDirectorio){
         File dir = new File(rutaDirectorio);
         if(!dir.exists()){
@@ -32,54 +145,5 @@ public class Main {
             throw new RuntimeException(e);
         }
     }
-
-    public static void escribirSecuencial(String contenido,String ruta){
-        File f = new File(ruta);
-        try( FileWriter fw = new FileWriter(f,true)) {
-
-            char [] cadenaParaEscribir = contenido.toCharArray();
-
-            for (char caracter : cadenaParaEscribir) {
-                fw.write(caracter);
-            }
-                fw.write('\n');
-            fw.flush();
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    private static void escribirAleatorio(String escribir, String ruta) {
-
-        try(RandomAccessFile raf = new RandomAccessFile(ruta,"rw");) {
-            raf.seek(raf.length());
-            raf.writeBytes(escribir);
-            raf.writeBytes("\n");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static void leerArchivo(String rutaAlArchivo) {
-        try(RandomAccessFile raf = new RandomAccessFile(rutaAlArchivo,"r")){
-            raf.seek(0);
-            char[] contenidoArchivo = new char[(int) raf.length()];
-            for (int i = 0; i < raf.length() ; i++) {
-                contenidoArchivo[i] = raf.readChar();
-            }
-
-            //System.out.println(contenidoArchivo.toString());
-//            raf.seek(0);
-//            String contenidoString = raf.readUTF();
-//            System.out.println(contenidoString);
-
-            //raf.readChar();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 
 }
