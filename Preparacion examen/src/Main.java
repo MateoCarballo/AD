02b6197317407823fd96f4263bad8f1b34bae696
Mateo <mateocarballo2@gmail.com>
2024-10-15 18:27:34 +0200

    Completadas todas las lectura y escrituras simples. Pendientes lectura y escritura aleatorias y FileNameFilter

--- Preparacion examen/src/Main.java
+++ Preparacion examen/src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         final String NOMBRE_ARCHIVO_FILEWRITER = "Escrito con FileWriter.txt";
         final String NOMBRE_ARCHIVO_BUFFERWRITER = "Escrito con BufferWriter.txt";
         final String NOMBRE_ARCHIVO_DATAOUTPUTSTREAM = "Escrito con DataOutputStream.txt";
+        final String NOMBRE_ARCHIVO_OBJECTOUTPUTSTREAM = "Escrito con ObjectOutputStream.dat";
 
         //CREACION DEL DIRECTORIO
         crearDirectorio(RUTA);
@@ -17,17 +18,44 @@ public class Main {
         crearFichero(RUTA,NOMBRE_ARCHIVO_FILEWRITER);
         crearFichero(RUTA,NOMBRE_ARCHIVO_BUFFERWRITER);
         crearFichero(RUTA,NOMBRE_ARCHIVO_DATAOUTPUTSTREAM);
+        crearFichero(RUTA,NOMBRE_ARCHIVO_OBJECTOUTPUTSTREAM);
 
-        //ESCRIBIR ARCHIVOS CON FILEWRITER
+        //ESCRIBIR ARCHIVOS CON 
         escribirArchivoFileWriter(RUTA + File.separator + NOMBRE_ARCHIVO_FILEWRITER );
         escribirArchivoBufferedWriter(RUTA + File.separator + NOMBRE_ARCHIVO_BUFFERWRITER);
         escribirArchivoDataOutputStream(RUTA + File.separator + NOMBRE_ARCHIVO_DATAOUTPUTSTREAM);
+        escribirArchivoObjectOutputStream(RUTA + File.separator + NOMBRE_ARCHIVO_OBJECTOUTPUTSTREAM);
 
         //LEER ARCHIVOS CON LOS EQUIVALENTES A LA CLASE USADA PARA ESCRIBIR
 
         leerArchivoFileReader(new File(RUTA + File.separator + NOMBRE_ARCHIVO_FILEWRITER));
         leerArchivoBufferedReader(new File(RUTA + File.separator + NOMBRE_ARCHIVO_FILEWRITER));
         leerArchivoDataInputStream(new File(RUTA + File.separator + NOMBRE_ARCHIVO_FILEWRITER));
+        leerArchivoObjectInputStream(new File (RUTA + File.separator + NOMBRE_ARCHIVO_OBJECTOUTPUTSTREAM));
+    }
+
+    private static void leerArchivoObjectInputStream(File file) {
+        Persona objetoLeido = new Persona();
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))){
+            objetoLeido = (Persona) ois.readObject();
+            System.out.println(objetoLeido);
+        }catch (IOException | ClassNotFoundException e){
+            System.out.println("Capturada excepcion ");
+        }
+
+    }
+
+    private static void escribirArchivoObjectOutputStream(String ruta) {
+
+        Persona objetoEscrito = new Persona("nombre","apellidos",30);
+        File f = new File(ruta);
+
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));) {
+            oos.writeObject(objetoEscrito);
+            oos.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static void leerArchivoDataInputStream(File file) {
