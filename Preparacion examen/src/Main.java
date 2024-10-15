5b34feec697138f223757df50a5671a886428771
Mateo <mateocarballo2@gmail.com>
2024-10-15 19:12:24 +0200

    Completadas todas las lecturas y escrituras de binarios.

--- Preparacion examen/src/Main.java
+++ Preparacion examen/src/Main.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.SQLOutput;
 import java.util.Arrays;
 
@@ -10,6 +12,7 @@ public class Main {
         final String NOMBRE_ARCHIVO_BUFFERWRITER = "Escrito con BufferWriter.txt";
         final String NOMBRE_ARCHIVO_DATAOUTPUTSTREAM = "Escrito con DataOutputStream.txt";
         final String NOMBRE_ARCHIVO_OBJECTOUTPUTSTREAM = "Escrito con ObjectOutputStream.dat";
+        final String NOMBRE_ARCHIVO_RAF = "Escrito con RAF.txt";
 
         //CREACION DEL DIRECTORIO
         crearDirectorio(RUTA);
@@ -19,6 +22,7 @@ public class Main {
         crearFichero(RUTA,NOMBRE_ARCHIVO_BUFFERWRITER);
         crearFichero(RUTA,NOMBRE_ARCHIVO_DATAOUTPUTSTREAM);
         crearFichero(RUTA,NOMBRE_ARCHIVO_OBJECTOUTPUTSTREAM);
+        crearFichero(RUTA,NOMBRE_ARCHIVO_RAF);
 
         //ESCRIBIR ARCHIVOS CON 
         escribirArchivoFileWriter(RUTA + File.separator + NOMBRE_ARCHIVO_FILEWRITER );
@@ -26,15 +30,38 @@ public class Main {
         escribirArchivoDataOutputStream(RUTA + File.separator + NOMBRE_ARCHIVO_DATAOUTPUTSTREAM);
         escribirArchivoObjectOutputStream(RUTA + File.separator + NOMBRE_ARCHIVO_OBJECTOUTPUTSTREAM);
 
+        //ESCRITURA DE FORMA ALEATORIA 2 VECES SABIENDO CUANTO ESCRIBIMOS PARA SABER DESDE DONDE EMPEZAR
+        escribirArchivoRaf(RUTA + File.separator + NOMBRE_ARCHIVO_RAF,"Esta cadena esta escrita en la PRIMERA llamada al metodo.",0);
+        escribirArchivoRaf(RUTA + File.separator + NOMBRE_ARCHIVO_RAF,"Esta cadena esta escrita en la SEGUNDA llamada al metodo.",56);
+
         //LEER ARCHIVOS CON LOS EQUIVALENTES A LA CLASE USADA PARA ESCRIBIR
 
         leerArchivoFileReader(new File(RUTA + File.separator + NOMBRE_ARCHIVO_FILEWRITER));
         leerArchivoBufferedReader(new File(RUTA + File.separator + NOMBRE_ARCHIVO_FILEWRITER));
         leerArchivoDataInputStream(new File(RUTA + File.separator + NOMBRE_ARCHIVO_FILEWRITER));
         leerArchivoObjectInputStream(new File (RUTA + File.separator + NOMBRE_ARCHIVO_OBJECTOUTPUTSTREAM));
+        leerArchivoRAF(RUTA + File.separator + NOMBRE_ARCHIVO_RAF,0);
+        leerArchivoRAF(RUTA + File.separator + NOMBRE_ARCHIVO_RAF,56);
+    }
+
+    private static void leerArchivoRAF(String ruta, int valorDelSeek) {
+        System.out.println("*************************** LECTURA CON RAF ***********************");
+        try(RandomAccessFile raf = new RandomAccessFile(ruta,"rw")){
+            raf.seek(valorDelSeek);
+            int tamanoArchivo = (int) Files.size(Paths.get(ruta));
+            byte [] contenidoChars = new byte[tamanoArchivo];
+            raf.read(contenidoChars);
+            for (int c : contenidoChars) {
+                System.out.print((char) c);
+            }
+            System.out.print("\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static void leerArchivoObjectInputStream(File file) {
+        System.out.println("*************************** LECTURA CON OBJECTINPUTSTREAM ***********************");
         Persona objetoLeido = new Persona();
         try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))){
             objetoLeido = (Persona) ois.readObject();
@@ -61,9 +88,16 @@ public class Main {
     private static void leerArchivoDataInputStream(File file) {
 
         try (DataInputStream dis = new DataInputStream(new FileInputStream(file))){
-
             System.out.println("*************************** LECTURA CON DATAINPUTSTREAM ***********************");
-            dis.readChar();
+
+            int lectura = dis.read();
+
+            while(lectura != 46 && lectura != -1){
+                System.out.print((char) lectura);
+                lectura=dis.read();
+            }
+
+
         }catch (IOException e){
             System.out.println("Error durante la lectura");
         }
@@ -83,7 +117,7 @@ public class Main {
     }
 
     private static void leerArchivoFileReader(File file) {
-
+        System.out.println("*************************** LECTURA CON FILEREADER ***********************");
         try(FileReader fr = new FileReader(file)) {
             int lecturaEnInt = fr.read();
             System.out.print((char)lecturaEnInt);
@@ -100,6 +134,21 @@ public class Main {
 
 
     }
+
+
+    private static void escribirArchivoRaf(String ruta,String contenido, int valorDelSeek) {
+        try (RandomAccessFile raf = new RandomAccessFile(ruta,"rw")){
+            raf.seek(valorDelSeek);
+            char[] contenidoChars = contenido.toCharArray();
+            for (int i = 0; i < contenidoChars.length; i++) {
+                raf.write((byte) contenidoChars[i]);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 
     private static void escribirArchivoDataOutputStream(String ruta) {
         File f = new File(ruta);
