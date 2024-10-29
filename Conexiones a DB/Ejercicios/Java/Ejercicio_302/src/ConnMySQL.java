2bb96b19bbf9c370df86b47b1f23e29c5b4bbb34
Mateo <mateocarballo2@gmail.com>
2024-10-29 11:57:43 +0100

    Completada tarea 302 Bases de datos

--- Conexiones a DB/Ejercicios/Java/Ejercicio_302/src/ConnMySQL.java
+++ Conexiones a DB/Ejercicios/Java/Ejercicio_302/src/ConnMySQL.java
@@ -3,14 +3,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnMySQL {
-    private static Connection conn;
+    static Connection conn;
     private final String usuario = "root";
     private final String clave = "abc123.";
     private final String url = "jdbc:mysql://localhost:3306/Empleados";
 
     private ConnMySQL(){
         try{
-            this.conn = DriverManager.getConnection(url,usuario,clave);
+            conn = DriverManager.getConnection(url,usuario,clave);
         }catch (SQLException e){
             System.out.println(e.getMessage());
         }
