d3af121e28ab8059c6c1706fd74a0c672a29e251
Mateo <mateocarballo2@gmail.com>
2024-11-29 10:50:30 +0100

    Eliminados comentarios para probar los metodos de modelo directamente

--- Conexiones a DB/Entregable/Entregable_DB/src/Main.java
+++ Conexiones a DB/Entregable/Entregable_DB/src/Main.java
@@ -23,19 +23,5 @@ public class Main {
                 e.printStackTrace();
             }
         }
-
-
-        //m.crearCategoria(" "); //1
-        //m.crearProveedor("Nombre prueba proveedor","12345678A",123456789,"email@deprueba.com"); //2
-        //m.eliminarProveedor(2);        //3
-        //m.crearUsuario("Nombre prueba","email@prueba.com",2024);      //4
-        //m.eliminarUsuario(4);     //5
-        //m.crearProducto("nombre producto prueba",99.99,1000,"Monitor","67890123A"); //6
-        //m.eliminarProductoPorNombre("Laptop Lenovo"); //7
-        //m.listarProductosBajoStock(50); //8
-        //m.obtenerTotalPedidosUsuarios(); //9
-        //m.obtenerCantidadProductosEnCadaAlmacen(); //10
-        //m.listarTodosProductosConCategoriaYProveedor(); //11
-        //m.obtenerUsuariosCompraronProductosCategoria(1); //12
     }
 }
