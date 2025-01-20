e85766ae97f4712a4df22b7210e3ff6e9130153d
MateoPortatil <mateocarballo2@gmail.com>
2025-01-20 17:21:31 +0100

    Completada plantilla hibernate-lombok-mysql

--- Evaluacion 02/Plantilla/Plantilla/src/main/java/org/example/App.java
+++ Evaluacion 02/Plantilla/Plantilla/src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.hibernate.Session;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Test");
+
+        Session session = HibernateUtil.get().openSession();
+
+        session.close();
+        System.out.println("Finalizando la conexion a MySQL");
     }
 }
