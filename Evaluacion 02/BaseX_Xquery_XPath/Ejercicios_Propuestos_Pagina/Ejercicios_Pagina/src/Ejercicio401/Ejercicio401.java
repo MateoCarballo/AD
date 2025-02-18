2bea9f1b2efd3ff6a3578bd7f59d3e5cbffaab5b
Mateo <mateocarballo2@gmail.com>
2025-02-18 19:50:55 +0100

    .

--- Evaluacion 02/BaseX_Xquery_XPath/Ejercicios_Propuestos_Pagina/Ejercicios_Pagina/src/Ejercicio401/Ejercicio401.java
+++ Evaluacion 02/BaseX_Xquery_XPath/Ejercicios_Propuestos_Pagina/Ejercicios_Pagina/src/Ejercicio401/Ejercicio401.java
@@ -2,12 +2,30 @@ package Ejercicio401;
 
 import org.basex.examples.api.BaseXClient;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.Buffer;
+import java.util.Scanner;
 
 public class Ejercicio401 {
+    final static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sbuilder = new StringBuilder();
+        sbuilder.append("<alumno>\n");
+        sbuilder.append("<nombre>");
+        sbuilder.append("nombre");
+        sbuilder.append("</nombre>\n");
+        sbuilder.append("<apellidos>");
+        sbuilder.append("apellidos apellidos");
+        sbuilder.append("</apellidos>\n");
+        sbuilder.append("<edad>");
+        sbuilder.append("edad");
+        sbuilder.append("</edad>\n");
+        sbuilder.append("<correo>");
+        sbuilder.append("correo");
+        sbuilder.append("</correo>\n");
+        sbuilder.append("</alumno>\n");
+        String prueba = sbuilder.toString();
         //Crear la db Ejercicio 401
         //Iterar sobre objetos para insertarlos
         /*
@@ -18,11 +36,78 @@ public class Ejercicio401 {
             <correo></correo>
          </alumno>
          */
+
+        //Crear base de datos si lo necesita
         //crearDBBaseX();
+
+        //Eliminar base de datos si lo necesitas
         //eliminarBaseX();
-        insertarFichero();
+
+        do{
+            crearAlumnoParaInsertar();
+            //hardcodeado para poder probar
+            //insertarFichero("Ejercicio401",prueba);
+            System.out.println("Seguir introduciendo Si/No");
+        }while((scanner.next().equalsIgnoreCase("Si")));
+
+        System.out.println("""
+                Una vez finalizada la introducción de datos el sistema mostrará un menú con las siguientes opciones:
+                1 -> Número total de documentos en el sistema
+                2 ->Media de edad de los alumnos.
+                3 ->Mostrar un XML con la edad del alumno mayor y del alumno menor (todo en el mismo documento XML).
+                4 ->Mostrar el nombre de los alumnos ordenado por edad de mayor a menor.
+                5 ->Mostrar el nombre de un alumno de forma aleatoria.
+          
+                """);
+        int eleccion = 0;
+        try{
+            eleccion= Integer.parseInt(br.readLine());
+        }catch (Exception e){
+            System.out.println("Error al elegir en el menu");
+        }
+        switch (eleccion){
+            case 1 -> {numeroTotalDocumentos()}
+            case 2 -> {mediaEdadAlumnos()}
+            case 3 -> {mostrarXMLEdadAlumnoMayor()}
+            case 4 ->{mostrarAlumnosEdadMayorMenor()}
+            case 5 ->{mostrarAlumnoAleatorio()}
+            default -> {}
+        }
+    }
+
+    private static void crearAlumnoParaInsertar() {
+        StringBuilder sb = new StringBuilder();
+        try{
+            sb.append("<alumno>\n");
+            System.out.println("Nombre");
+            sb.append("<nombre>");
+            String nombre = br.readLine();
+            sb.append(nombre);
+            sb.append("</nombre>\n");
+            System.out.println("Apellidos");
+            sb.append("<apellidos>");
+            String apellidos = br.readLine();
+            sb.append(apellidos);
+            sb.append("</apellidos>\n");
+            System.out.println("Edad");
+            sb.append("<edad>");
+            String edad = br.readLine();
+            sb.append(edad);
+            sb.append("</edad>\n");
+            System.out.println("Correo");
+            sb.append("<correo>");
+            String correo = br.readLine();
+            sb.append(correo);
+            sb.append("</correo>\n");
+            sb.append("</alumno>");
+        }catch (IOException e){
+            System.out.println("Error al leer");
+        }
+
+        insertarFichero("Fichero401",sb.toString());
 
     }
+
     public static void crearDBBaseX(){
         try(BaseXClient session = new BaseXClient(
                 "localhost",
@@ -54,7 +139,7 @@ public class Ejercicio401 {
         }
     }
 
-    private static void insertarFichero(){
+    private static void insertarFichero(String nombreDB, String Alumno){
         try(BaseXClient session = new BaseXClient(
                 "localhost",
                 1984,
@@ -64,12 +149,13 @@ public class Ejercicio401 {
         ){
             //Apertura base de datos
             session.execute("open Ejercicio401");
-            final InputStream bais = new ByteArrayInputStream("<xml>Ejercicio401</xml>".getBytes());
+            final InputStream bais = new ByteArrayInputStream(Alumno.getBytes());
 
             //Insertar un archivo .xml
-            session.add("src/Ejercicio401/Alumno.xml",bais);
+            session.add(nombreDB + "/Alumno.xml",bais);
         } catch (IOException e) {
             System.out.println("Error al añadir fichero");
+            e.printStackTrace();
         }
 
     }
